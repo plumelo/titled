652b5e30fffcb7eccee69adf445cddf5163f22b0
Meghea Iulian <iulian.meghea@gmail.com>
2023-11-26 22:41:35 +0200

    feat: convert to lightningcss

---
+++ src/lightning.js
@@ -0,0 +1,43 @@
+const block = (declarations) => ({
+  type: "style",
+  value: {
+    selectors: [[{ type: "nesting" }]],
+    declarations: { declarations },
+    rules: [],
+    loc: { source_index: 0, line: 0, column: 0 },
+  },
+});
+
+const textDeclarations = (prefix) => [
+  {
+    property: "font-size",
+    raw: `var(--${prefix}-size)`,
+  },
+  {
+    property: "line-height",
+    raw: `var(--${prefix}-lh)`,
+  },
+];
+
+export const text = (rule) => block(textDeclarations(rule.prelude.value));
+export const heading = (rule) =>
+  block([
+    ...textDeclarations(rule.prelude.value),
+    {
+      property: "letter-spacing",
+      raw: `var(--${rule.prelude.value}-ls)`,
+    },
+  ]);
+
+export const customAtRules = {
+  text: {
+    prelude: "<custom-ident>",
+  },
+  heading: {
+    prelude: "<custom-ident>",
+  },
+};
+
+export const visitor = {
+  Rule: { custom: { text, heading } },
+};
